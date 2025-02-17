55da767d40451d24d9bf10122cd4656882395361
YEOP <80512800+lsy980326@users.noreply.github.com>
2025-02-17 15:47:20 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 34.94 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int[][] queries) {
+        StringBuilder answer = new StringBuilder(my_string);
+        Stack<Character> temp = new Stack<>();
+
+        for(int[] query:queries)
+        {
+            String subStr = answer.substring(query[0], query[1]+1); 
+            for (char s : subStr.toCharArray()) temp.push(s);
+            for(int i=query[0];i<=query[1];i++)
+            {
+                answer.setCharAt(i,temp.peek());
+                temp.pop();
+            }  
+        }
+        
+        return answer.toString();
+    }
+}
